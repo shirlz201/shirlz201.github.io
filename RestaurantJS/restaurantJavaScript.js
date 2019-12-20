736ffa8fb27c2160477898bf5804802b6836edea
Shirley Sosa <slaymesosa@gmail.com>
2019-12-20 15:07:26 -0500

    added drink to menu

--- RestaurantJS/restaurantJavaScript.js
+++ RestaurantJS/restaurantJavaScript.js
@@ -7,13 +7,3 @@ function validate() {
     console.log(name);
     console.log(email);
 }
-
-if (name== " " || !(isNan(name)) ) {
-    alert ("Please enter a valid name.")
-    return false;
-}
-
-    else if (name == "" || !(isNaN(name)) ) {
-    alert("Please enter a valid email.")
-    return false;
-    }
